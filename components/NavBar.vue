75a544e9e9ac3cba8340ad4b5852109d8d0f206c
Ronald <ronald-farias@outlook.com>
2018-08-23 19:14:01 -0300

    imported logo to navbar

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,13 +1,35 @@
 <template>
-  <nav></nav>
+<div>
+
+  <nav class="navbar">
+    <div class="navbar-brand">
+      <a href="" class="navbar-item"></a>
+
+<logo />
+    </div>
+
+  </nav>
+
+
+
+
+
+</div>
 </template>
 
 <script>
+import logo from '~/components/IlhacastLogo'
 export default {
+  components: {
+    logo
+  },
   data () {
     return {
 
     }
-  }
+  },
+  mounted () {
+
+  } // end mounted
 }
 </script>
