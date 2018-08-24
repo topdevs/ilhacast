2b2bdf731540bcdca931ed20db866bef46b12fc4
Ronald <ronald-farias@outlook.com>
2018-08-24 17:31:52 -0300

    added font-family

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,21 +1,66 @@
 <template>
-<div>
-
-  <nav class="navbar">
-    <div class="navbar-brand">
-      <a href="" class="navbar-item"></a>
-
-<logo />
+  <div class="container-flex container-shadow">
+    <div class="container-item">
+      <p>O melhor, mais top e mais pica ! </p>
+    </div>
+    <div class="container-item">
+      <logo />
     </div>
 
-  </nav>
-
-
-
-
-
-</div>
+    <div class="container-item align-right">
+      <div class="">
+        <a href="https://github.com/topdevs/ilhacast">
+          <i class="fa fa-github-alt"> Github</i>
+        </a>
+      </div>
+    </div>
+  </div>
 </template>
+<style>
+  p {
+    font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+    /* font-style: italic; */
+  }
+
+  a {
+    text-decoration: none;  
+    color: black;
+  }
+  .container-shadow {
+    /* x y smoth strength */
+    box-shadow: 0px 1px 10px 0px; 
+    /* background-color: rgba(255, 255, 255, .9); */
+    -webkit-backdrop-filter: blur(100px);
+  }
+
+  .container-flex {
+    display: flex;
+    flex-wrap: wrap; 
+    flex-direction: row;
+    width: 100%;
+    /* padding 0 top 10 right 10 bottom 0 left */
+    padding: 0px 10px 10px 0px; 
+    align-items: center;
+    justify-content: center;
+    box-sizing: border-box;
+
+  }
+  .container-item {
+    /* display: flex; */
+    flex: 1;
+    text-align: center;
+    /* background: red; */
+  }
+  .align-right {
+    text-align: right;
+  }
+
+  .align-left {
+    text-align: left;
+  }
+
+</style>
+
 
 <script>
 import logo from '~/components/IlhacastLogo'
@@ -27,7 +72,8 @@ export default {
     return {
 
     }
-  },
+  },// end data
+
   mounted () {
 
   } // end mounted
