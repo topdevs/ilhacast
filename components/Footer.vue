5e811f9dfac137e07c4edcfab7c573865aecf4a9
Ronald <ronald-farias@outlook.com>
2018-08-24 17:27:51 -0300

    created footer component

---
+++ components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="footer">
+  <div class="container-flex">
+    <div class="container-item align-left">
+      <p>Links</p>
+    </div>
+    <div class="container-item">
+      <p>Fale conosco</p>
+    </div>
+    <div class="container-item">
+      <p>Code</p>
+    </div>
+  </div>
+</div>
+
+
+</template>
+
+<style>
+.footer {
+  
+  color: azure;
+  background-color: rgba(0, 0, 0 , 0.9);
+}
+
+</style>
