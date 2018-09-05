b01744dfa2e4537f54b4a5f4d0b742e89b95a137
Ronald <ronald-farias@outlook.com>
2018-09-04 22:07:35 -0300

    removed garbage, added personal global fonts

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="myApp">
-    <nav-bar class="bg-blur"></nav-bar>
+    <nav-bar></nav-bar>
     <nuxt/>
     <Footer />
   </div>
@@ -20,18 +20,54 @@ export default {
 </script>
 
 <style>
-.bg-blur{
-  backdrop-filter: invert();  
-  -webkit-backdrop-filter: invert();
-  background-color: rgba(93, 92, 95, .1);
-  
-}
+/* GLOBAL FONTS */
+  @font-face {
+    font-family: 'RobotoThin';
+    src: url('~static/fonts/Roboto-Thin.ttf');
+  }
+  @font-face {
+    font-family: 'OldeEnglish';
+    src: url('~static/fonts/OldeEnglish.ttf');
+  }
 
-#myApp {
-  /* background-image: url('/static/bg.jpg'); */
-  background-repeat: no-repeat;
-  background-position: center;
-  background-size: cover;
-}
+/* GLOBAL CSS CLASSES */
+  * {
+   margin: 0;
+   padding: 0;
+   font-family: RobotoThin;
+  }
+  .container-shadow {
+    box-shadow: 2px 1px 10px 2px;  /* x, y, smoth, strength */
+    -webkit-backdrop-filter: blur(100px);
+  }
+
+  .container-flex {
+    display: flex;
+    flex-wrap: wrap; 
+    flex-direction: row;
+    width: 100%;
+    padding: 0px 10px 10px 0px; 
+    align-items: center;
+    justify-content: center;
+    box-sizing: border-box;
+
+  }
+  .container-item {
+    flex: 1;
+    text-align: center;
+  }
+  .align-right {
+    text-align: right;
+  }
+
+  .align-left {
+    text-align: left;
+  }
+  #myApp {
+    /* background-image: url('/static/bg.jpg'); */
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+  }
 </style>
 
