f34b5552cfaee3cc37992961f53cffce712bfe0a
Ronald <ronald-farias@outlook.com>
2018-09-04 22:02:57 -0300

    fixed flexbox with list of links

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,26 +1,62 @@
 <template>
 <div class="footer">
   <div class="container-flex">
-    <div class="container-item align-left">
-      <p>Links</p>
+    <div class="container-item">
+      <p>Links úteis</p>
+      <br>
+      <ul>
+        <li>Podcasts</li>
+        <li>...</li>
+        <li>Link 3</li>
+      </ul>
     </div>
     <div class="container-item">
-      <p>Fale conosco</p>
+      <p>Quem somos</p>
+      <br>
+      <ul>
+        <li>Link 1</li>
+        <li>Link 2</li>
+        <li>Link 3</li>
+      </ul>
     </div>
     <div class="container-item">
       <p>Code</p>
+      <br>
+      <ul>
+        <li>Link 1</li>
+        <li>Link 2</li>
+        <li>Link 3</li>
+      </ul>
     </div>
   </div>
+
 </div>
 
 
 </template>
 
-<style>
+<style scoped>
+
+ul {
+  list-style: none;
+}
+
+.container-flex {
+  padding-top: 20px;
+  padding-left: 20px;
+  padding-bottom: 100px;
+  font-family: RobotoThin;
+}
+
+p {
+  font-weight: bold;
+}
+
 .footer {
-  
-  color: azure;
-  background-color: rgba(0, 0, 0 , 0.9);
+  color:azure;
+  background-color: rgba(0, 0, 0, .9);
+  position: absolute;
+  width: 100%;
 }
 
 </style>
