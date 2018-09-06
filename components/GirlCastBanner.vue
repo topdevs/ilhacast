6957093accf6dfe3157bebf0301458ae6c1e4677
Ronald <ronald-farias@outlook.com>
2018-09-05 23:40:34 -0300

    added GirlCastBanner layout

---
+++ components/GirlCastBanner.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <div>
+
+      <div class="text">
+        <h1>
+          <slot></slot>
+        </h1>
+      </div>
+
+    </div>  
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+}
+
+.text {
+  text-align: center;
+  flex: 1;
+}
+</style>
