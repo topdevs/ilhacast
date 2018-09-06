8f91e17a9b8c639547eca4f1fcecbc068d01efd4
Ronald <ronald-farias@outlook.com>
2018-09-06 03:10:24 -0300

    added flexbox sections content and coded their classes

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,35 @@
 <template>
   <div class="index">
-    <section class="section">
-      <girl-cast-banner>Aqui você encontra loucos convictos
-      </girl-cast-banner>
+    <section class="section">      
+      <p class="section__title--sample">
+        <a href="#hosts">#1</a>&nbsp;
+        <a class="h1">Frase de efeito</a>
+      </p>
+      
+      <br>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis cumque error perferendis nostrum fugit fugiat nulla optio atque? Reprehenderit sequi aliquid minima nihil et esse deleniti, fugit aperiam ullam quisquam.</p>    
     </section>
-     <section class="section">
-      <h1>teste</h1>
+    <section class="section bg-gray bg-sea">
+      <h1>Logos, um banner ou icones de tecnologias e parceiros talvez</h1>
+  
     </section>
+     <section id="hosts" class="section ">
+       <div class="section__title">
+        <h1>Hosts</h1>
+       </div>
+      <div class="section__hosts">
+      <div class="host__avatar" @click.native="alert('modal bacana (?)')">
+        <p class="host__avatar__subtitle">Jhon</p>
+      </div>
+      <div class="host__avatar" @click.native="alert('modal bacana (?)')">
+        <p class="host__avatar__subtitle">Doe</p>
+      </div>
+      <div class="host__avatar" @click.native="alert('modal bacana (?)')">
+        <p class="host__avatar__subtitle">...</p>
+      </div>
+    </div>
+    </section>
+    
   </div>
 </template>
 
@@ -27,10 +50,85 @@ export default {
   flex-direction: column;
 
 }
-
+.bg-gray {
+  background-color: rgb(232, 231, 231);
+}
+.bg-sea {
+  background: url('~static/bg.jpg');
+  background-position: center;
+  background-repeat: no-repeat; 
+  background-size: cover;
+  background-position-y: 0;
+}
+.bg-brain {
+  background: url('~static/abstractbrain.png');
+  background-position: center;
+  background-repeat: no-repeat; 
+  background-size: auto;
+  
+}
+.bg-circuits {
+  background: url('~static/circuitos-eletricos.png');
+  background-position: center;
+  background-repeat: no-repeat; 
+  background-size: cover;
+  color: azure;
+  
+}
+.h1 {
+  font-size: 24px;
+  font-weight: bold;
+}
 .section {
+  padding: 40px;
   flex: 1;
+  display: flex;
+  flex-direction: column; 
+  justify-content: center;
+  align-items: center;
+  align-content: center;
+  text-align: center;
+  height: 250px;
+  transition: .5s ease;
+}
+.section:hover {
+  height: 300px;
+}
+.section__title--sample a {
+  text-decoration: none;
+  color: black;
+}
+.section__title {
+  flex: 1;
+  display: flex;
+  align-items: center;
+}
+.section__hosts {
+  padding-bottom: 30px; 
+  height: 250px;
+  flex: 1;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+.host__avatar {
+  margin-left: 50px;
+  background: gray;
+  width: 150px;
+  height: 150px;
+  border-radius: 50%;
+  cursor: pointer;
+  background: url('~static/Circuit-Man-Head.png');
+  background-position: center;
+  background-repeat: no-repeat; 
+  background-size: contain;
+  box-shadow: 1px 1px 5px tomato;
 }
 
+.host__avatar__subtitle {
+  position: relative;
+  text-align: center;
+  top: 70px;
+}
 </style>
 
