21274ae605b900ede5ff865b6a6478de95d1c244
Ronald <ronald-farias@outlook.com>
2018-09-05 23:42:15 -0300

    added girlBanner component and organize section flexbox

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,36 @@
 <template>
-  <section style="height: 30vh">
-    <h1></h1>
-  </section>
+  <div class="index">
+    <section class="section">
+      <girl-cast-banner>Aqui você encontra loucos convictos
+      </girl-cast-banner>
+    </section>
+     <section class="section">
+      <h1>teste</h1>
+    </section>
+  </div>
 </template>
 
 <script>
+import GirlCastBanner from '~/components/GirlCastBanner'
 
 export default {
   components: {
-    
+    GirlCastBanner
   }
 }
 </script>
 
-<style>
+<style scoped>
+.index {
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+
+}
+
+.section {
+  flex: 1;
+}
 
 </style>
 
